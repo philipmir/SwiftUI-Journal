4654f894e3531b76e1e4ed71a5f40c5ce83274c1
philipmir <philipmir@hotmail.com>
2023-04-09 11:42:15 +0200

    Initial Commit

---
+++ SwiftUI-Journal/SwiftUI_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JournalApp.swift
+//  SwiftUI-Journal
+//
+//  Created by Philip Miranda on 2023-04-09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
